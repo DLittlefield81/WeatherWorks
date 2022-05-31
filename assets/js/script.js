72127989a09cac3788f5be8cdbf9e0e0694d4d58
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-05-31 08:26:42 -0400

    variables working, started bootstrap, added units dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,9 @@ let app = {
     fetchWeather: () => {
         //Values from Inputs
         let searchCity = document.getElementById('searchCity').value;
+        let units = document.getElementById('units').value;
         let APIKey = "41d95db3e8a81f9edfd2e2e5a2abf949";
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${APIKey}`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=${units}&appid=${APIKey}`;
         fetch(url)
             .then(resp => {
                 if (!resp.ok) throw new Error(resp.statusText);
@@ -20,30 +21,26 @@ let app = {
             })
             .catch(console.err);
     },
-    showWeather: (resp) => {
-        console.log(resp); //Incoming Data
-        let row = document.querySelector('.weather.row');
-        row.innerHTML =  resp
-        //     .map((day, numberOfDays) => {
-        //         if (numberOfDays <= 4) {
-        //           return 
-        `<div class="card-body">
-        <h3 class = "card-title" > Weather Label < /h3> <
-            p class = "card-text" > High Temp Low Temp < /p> <
-            p class = "card-text" > HighFeels like < /p> <
-            p class = "card-text" > Pressure < /p> <
-            p class = "card-text" > Humidty < /p> <
-            p class = "card-text" > UV Index < /p> <
-            p class = "card-text" > Precipitation < /p> <
-            p class = "card-text" > Dew Point < /p> <
-            p class = "card-text" > Wind speed and direction < /p> <
-            p class = "card-text" > Sunrise < /p> <
-            p class = "card-text" > Sunset < /p> <
-            /div>`;
-        //        };
+    showWeather: (weatherData) => {
+        console.log(weatherData); //Incoming Data
+        //Extract Data
+        let { name } = weatherData;
+        let {
+            icon,description
+        } = weatherData.weather[0];
+        let {
+            temp,
+            humidity
+        } = weatherData.main;
+        let {
+            speed
+        } = weatherData.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        
+
 
     }
 }
-// .join(' ');
+
 
 app.init();
