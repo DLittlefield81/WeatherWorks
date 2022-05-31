3a5649e503a151a4a7a6a05e9012a38bcf32545c
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-05-30 20:23:05 -0400

    roughed in HTML layout for js

---
+++ README.md
@@ -0,0 +1,2 @@
+GIT PAGES: https://dlittlefield81.github.io/WeatherWorks/
+GIT HUB: https://github.com/DLittlefield81/WeatherWorks.git
---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+    // API KEY From Website
+let APIKey = "41d95db3e8a81f9edfd2e2e5a2abf949";
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;    
