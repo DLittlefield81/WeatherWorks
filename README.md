e42b67c133cbff00271a929c2bb9ce3c295f0470
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-06-01 09:08:40 -0400

    uv color coding with switch statement functions. need to create cards for daily results

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-GIT PAGES: https://dlittlefield81.github.io/WeatherWorks/
-GIT HUB: https://github.com/DLittlefield81/WeatherWorks.git
+# WeatherWorks
+
+## Description
+Are you trying to find out if you should wear shorts or bring that umbrella? WeatherWorks is your ticket to getting your day off to a good start! 
+Enter your city and we'll tell you your current forecast as well as the next 5.
+We'll even tell you if you should put on sunscreen with our new UV index with color advisor!
+
+## Installation
+
+Follow these steps to install this project on your local machine.
+
+- Open Bash editor
+- mkdir weatherworks
+- cd weatherworks
+- git clone https://github.com/DLittlefield81/WeatherWorks.git
+
+## Application
+
+Link to Deployed Application: https://dlittlefield81.github.io/WeatherWorks/
+
+
+![WordWise Trivia Game](assets/images/weatherworks.png)
+
+
+## License
+
+Non-Software Licenses
+Open source software licenses can be also used for non-software works and are often the best choice, especially when the works in question can be edited and versioned as source (e.g., open source hardware designs). Choose an open source license here.
+
+Data, media, etc.
+CC0-1.0, CC-BY-4.0, and CC-BY-SA-4.0 are open licenses used for non-software material ranging from datasets to videos. Note that CC-BY-4.0 and CC-BY-SA-4.0 should not be used for software.
+
+Documentation
+Any open source software license or open license for media (see above) also applies to software documentation. If you use different licenses for your software and its documentation, be sure to specify that source code examples in the documentation are also licensed under the software license.
+
+Fonts
+The SIL Open Font License 1.1 keeps fonts open, allowing them to be freely used in other works.
+
+Mixed projects
+If your project contains a mix of software and other material, you can include multiple licenses, as long as you are explicit about which license applies to each part of the project. See the license notice for this site as an example.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+//const clearHistory = document.getElementById("#btnClearhistory");
+let searchCity = document.getElementById('searchCity').value;
+const uviEl = document.getElementById("uvi");
+let APIKey = "41d95db3e8a81f9edfd2e2e5a2abf949";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&appid=${APIKey}`;
+
+//START APP
 let app = {
     init: () => {
         document
@@ -5,11 +12,8 @@ let app = {
             .addEventListener('click', app.fetchWeather);
     },
     fetchWeather: () => {
-        //Values from Inputs
-        let searchCity = document.getElementById('searchCity').value;
-        let units = document.getElementById('units').value;
-        let APIKey = "41d95db3e8a81f9edfd2e2e5a2abf949";
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=${units}&appid=${APIKey}`;
+        //        localStorage.setItem("search", JSON.stringify(searchHistory));
+        console.log(url)
         fetch(url)
             .then(resp => {
                 if (!resp.ok) throw new Error(resp.statusText);
@@ -22,24 +26,99 @@ let app = {
             .catch(console.err);
     },
     showWeather: (weatherData) => {
+
         console.log(weatherData); //Incoming Data
         //Extract Data
-        let {name} = weatherData;
-        let {icon,description} = weatherData.weather[0];
-        let {temp,humidity} = weatherData.main;
-        let {speed} = weatherData.wind;
-        console.log(name, icon, description, temp, humidity, speed);
-        
-        document.querySelector("#city").innerHTML = name;
+
+        let {
+            name
+        } = weatherData;
+        let {
+            lon,
+            lat
+        } = weatherData.coord;
+        let {
+            icon,
+            description
+        } = weatherData.weather[0];
+        let {
+            temp,
+            humidity
+        } = weatherData.main;
+        let {
+            speed
+        } = weatherData.wind;
+        //Pull Data from weatherData
+        console.log(name, lon, lat, icon, description, temp, humidity, speed);
+        //Set Data to HTML
+        document.querySelector("#city").innerHTML = "Current Weather in " + name;
         document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector("#description").innerHTML = description;
-        document.querySelector("#temp").innerHTML = temp;
-        document.querySelector("#humidity").innerHTML = humidity + "%";
-        document.querySelector("#wind").innerHTML = wind;
+        document.querySelector("#temp").innerHTML = "Temperature: " + Math.round(temp) + "°C";
+        document.querySelector("#humidity").innerHTML = "Humidity: " + humidity + "%";
+        document.querySelector("#wind").innerHTML = "Wind Speed: " + Math.round(speed) + " km/h";
+        //Set UVQueryURL 
+        let uvQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+        fetch(uvQueryURL)
+            .then(resp => {
+                if (!resp.ok) throw new Error(resp.statusText);
+                return resp.json();
+            })
+            .then(data => {
+                app.showUV(data);
+            })
+            .catch(console.err);
+    },
+    showUV: (uvData) => {
+        console.log(uvData)
+        let {
+            uvi
+        } = uvData.current;
+        
+        
+        //UV Scale 1-2 Low GREEN //3-5 MED YELLOW //6-7 High ORANGE //8-10 Very High RED // 11+ EXTREME PURPLE
+         uvi = Math.round(uvi)
+        switch (uvi) {
+            case 0:
+            case 1:
+            case 2:
+                uviEl.innerHTML = "UV Index: " + uvi + " Low";
+                uviEl.setAttribute("class", "badge badge-success");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                uviEl.innerHTML = "UV Index: " + uvi + " Medium";
+                uviEl.setAttribute("class", "badge badge-warning");
+                break;
+            case 6:
+            case 7:
+               uviEl.innerHTML = "UV Index: " + uvi + " High";
+               uviEl.setAttribute("class", "badge badge-danger");
+                break;
+            case 8:
+            case 9:
+            case 10:
+                uviEl.innerHTML = "UV Index: " + uvi + " Very High";
+                uviEl.setAttribute("class", "badge badge-info");
+                break;
+            case 11:
+               uviEl.innerHTML = "UV Index: " + uvi + " Extreme";
+               uviEl.setAttribute("class", "badge badge-primary");
+                break;
+        }
+        console.log("Current UV After Rounding: " + uvi)
+        
+        //uviEl.append(uvi);
+        //document.querySelector("#uvi").innerHTML = "UV Index: " + Math.round(uvi);
         
 
     }
+    // // Clear History button
+    // clearHistory.addEventListener("click", function () {
+    //     localStorage.clear();
+    //     searchHistory = [];
+    //     renderSearchHistory();
+    // })
 }
-
-
 app.init();
